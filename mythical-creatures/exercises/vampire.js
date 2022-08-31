06e4d6530016c5c9512155db2312314f24628777
Adam Hughes <adamjordanhughes@gmail.com>
2022-08-31 10:48:17 -0400

    Add vampire functionality

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,20 @@
+class Vampire {
+  constructor(name,pet='bat',thirsty=true,ouncesDrank=0){
+    this.name= name
+    this.pet = pet
+    this.thirsty=thirsty
+    this.ouncesDrank=ouncesDrank
+  }
+  drink(){
+    if (this.ouncesDrank >= 50) {
+      return 'I\'m too full to drink anymore!'
+    } else {
+      this.thirsty=false
+      this.ouncesDrank+=10
+    }
+  }
+
+
+}
+
+module.exports = Vampire;
