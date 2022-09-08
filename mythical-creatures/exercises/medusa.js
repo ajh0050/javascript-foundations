c685564f5db2790bd3aa0e4d9f42b5a6709b5891
Adam Hughes <adamjordanhughes@gmail.com>
2022-09-08 17:29:43 -0400

    Complete Medusa functionality)

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,19 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name, statues = []){
+    this.name = name
+    this.statues = statues
+  }
+  gazeAtVictim(person){
+    if (this.statues.length >2) {
+      var unstonedPerson = new Person(this.statues[0].name, 'relieved')
+      this.statues.shift()
+    }
+    this.statues.push(new Statue(person.name))
+    return unstonedPerson
+  }
+}
+
+module.exports = Medusa;
